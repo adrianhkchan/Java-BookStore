7f09bcd837c300d837a63a2614b1a60faa233599
Adrian Chan <adrianhkchan@gmail.com>
2019-01-20 22:57:28 +0000

    Update books in database

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -67,6 +67,9 @@ public class ControllerServlet extends HttpServlet {
                 case "/edit":
                     showEditForm(request, response);
                     break;
+                case "/update":
+                    updateBook(request, response);
+                    break;
                 default:
                     listBooks(request, response);
                     break;
@@ -125,6 +128,16 @@ public class ControllerServlet extends HttpServlet {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
         request.setAttribute("book", editBook);
         dispatcher.forward(request, response);
+    }
+
+    private void updateBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String title = request.getParameter("booktitle");
+        String author = request.getParameter("bookauthor");
+        String priceString = request.getParameter("bookprice");
+        Book updateBook = new Book(id, title, author, Float.parseFloat(priceString));
+        bookDAO.updateBook(updateBook);
+        response.sendRedirect("list");
     }
 
     /**
